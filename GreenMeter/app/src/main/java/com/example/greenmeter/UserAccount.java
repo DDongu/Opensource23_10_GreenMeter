8baecf897f6ff58d4274b8187f7993d3d56e1440
KimDongGyu <ehdrn1205@gmail.com>
2023-05-10 23:38:13 +0900

    firebase connect & make Login/Register

---
+++ GreenMeter/app/src/main/java/com/example/greenmeter/UserAccount.java
@@ -0,0 +1,37 @@
+package com.example.greenmeter;
+
+/**
+ * 사용자 계정 정보 모델 클래스
+ */
+
+public class UserAccount {
+    private String idToken; // Firebase Uid (고유 토큰정보)
+    private String emailId; // 이매일 아이디
+    private String password; // 비밀번호
+
+    public UserAccount() { } // firebase에서는 무조건 빈 생성자가 있어야 됨
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+}
