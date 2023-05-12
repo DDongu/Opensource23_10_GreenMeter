ab76b05a7bde23a86fbf26b0fad16603b4ac023f
KimDongGyu <ehdrn1205@gmail.com>
2023-05-13 00:06:47 +0900

    Make Bottom NaviBar

---
+++ GreenMeter/app/src/main/java/com/example/greenmeter/Lanking.java
@@ -0,0 +1,22 @@
+package com.example.greenmeter;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Lanking extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.lanking, container, false);
+        return view;
+    }
+}
--- GreenMeter/app/src/main/java/com/example/greenmeter/MainActivity.java
+++ GreenMeter/app/src/main/java/com/example/greenmeter/MainActivity.java
@@ -1,36 +1,87 @@
 package com.example.greenmeter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mFirebaseAuth;
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Greenmeter greenmeter;
+    private Timeline timeline;
+    private Lanking lanking;
+    private Mypage mypage;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_logout = findViewById(R.id.button_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                // 로그아웃 하기
-                mFirebaseAuth.signOut();
-
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.tab_greenmeter:
+                        setFrag(0);
+                        break;
+                    case R.id.tab_timeline:
+                        setFrag(1);
+                        break;
+                    case R.id.tab_lanking:
+                        setFrag(2);
+                        break;
+                    case R.id.tab_mypage:
+                        setFrag(3);
+                        break;
+                }
+                return true;
             }
         });
-//        //탈퇴처리
-//        mFirebaseAuth.getCurrentUser().delete();
+
+        greenmeter = new Greenmeter();
+        timeline = new Timeline();
+        lanking = new Lanking();
+        mypage = new Mypage();
+        setFrag(0); // 첫 프래그먼트 화면 지정
     }
+
+    //프래그먼트 교체가 이러는 곳
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, greenmeter);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, timeline);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, lanking);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, mypage);
+                ft.commit();
+                break;
+        }
+    }
+
 }
