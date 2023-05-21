43d4b44eee3d3381b489d723f2f9c092c1354c73
KimDongGyu <ehdrn1205@gmail.com>
2023-05-21 20:13:01 +0900

    Modify Timeline: update user location at interval

--- GreenMeter/app/src/main/java/com/example/greenmeter/Greenmeter.java
+++ GreenMeter/app/src/main/java/com/example/greenmeter/Greenmeter.java
@@ -1,26 +1,122 @@
 package com.example.greenmeter;
 
-/**
- * Greenmeter Main
- **/
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-public class Greenmeter extends Fragment {
+public class Greenmeter extends Fragment implements OnMapReadyCallback {
 
     private View view;
+    private GoogleMap mMap;
+    private MapView mapView;
+    private FusedLocationProviderClient client;
+    private LocationCallback locationCallback;
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.greenmeter, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = view.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        // 위치 업데이트 요청
+        client = LocationServices.getFusedLocationProviderClient(requireContext());
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    // 위치 업데이트 처리
+                }
+            }
+        };
+
+        // 권한 요청
+        checkPermission();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // 초기 위치 설정
+        double initialLatitude = 36.6283933; // 사용자의 초기 위도 값
+        double initialLongitude = 127.459223; // 사용자의 초기 경도 값
+        float DEFAULT_ZOOM = 15.0f; // 지도의 초기 줌 레벨
+
+        LatLng initialLocation = new LatLng(initialLatitude, initialLongitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, DEFAULT_ZOOM));
+
+
+        // 현재 위치 표시 설정
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        startLocationUpdates();
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            } else {
+                // 권한이 거부되었을 때 처리
+            }
+        }
+    }
+
+    private void startLocationUpdates() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
     }
 }
--- GreenMeter/app/src/main/java/com/example/greenmeter/Timeline.java
+++ GreenMeter/app/src/main/java/com/example/greenmeter/Timeline.java
@@ -4,19 +4,19 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.greenmeter.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,90 +24,110 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Timeline extends Fragment implements GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener, OnMapReadyCallback, LocationListener {
+import org.jetbrains.annotations.Nullable;
 
+
+public class Timeline extends Fragment implements OnMapReadyCallback, LocationListener {
     private View view;
     private GoogleMap mMap;
     private LocationManager locationManager;
+    private FusedLocationProviderClient fusedLocationClient;
     private final int MIN_TIME = 1000;
     private final int MIN_DISTANCE = 5;
-    private final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout and initialize the map
         view = inflater.inflate(R.layout.timeline, container, false);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // Initialize the FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
+        // Initialize the LocationManager
+        locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+
         return view;
     }
 
-    // NULL이 아닌 GoogleMap 객체를 파라미터로 제공해 줄 수 있을 때 호출
-    @Override
-    public void onMapReady(final GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setOnMyLocationButtonClickListener(this);
-        mMap.setOnMyLocationClickListener(this);
-
-        // 위치 정보 관리자
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        // 권한 확인
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-            return;
-        }
-
-        // 위치 업데이트 요청
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-
-        // 마커 추가
+        // Add a marker in CBNU
         LatLng myLocation = new LatLng(36.6283933, 127.459223);
-        mMap.addMarker(new MarkerOptions().position(myLocation).title("내 위치"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 10));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(myLocation)
+                .title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 17));
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void onResume() {
+        super.onResume();
+        // Request location updates when the fragment is resumed
+        startLocationUpdates();
+    }
 
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // 권한 허가됨
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-                }
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop location updates when the fragment is paused
+        stopLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        // Check for location permission
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Request location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+            // Get the last known location and update the map
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastKnownLocation != null) {
+                LatLng latLng = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                // Update the map with the last known location
+                updateMapWithLocation(latLng);
             }
+        } else {
+            // Location permission not granted, handle accordingly
         }
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        // 위치가 변경되었을 때 호출됨
-        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(myLocation).title("내 위치"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        // 내 위치 버튼이 클릭되었을 때 호출됨
-        return false;
-    }
-
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
-        // 내 위치가 클릭되었을 때 호출됨
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        // 위치 업데이트 중단
+    private void stopLocationUpdates() {
+        // Stop location updates
         locationManager.removeUpdates(this);
     }
+
+    private void updateMapWithLocation(LatLng latLng) {
+        if (mMap != null) {
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(latLng).title("My Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+        }
+    }
+
+    // Implement LocationListener methods
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        // Update the map with the new location
+        updateMapWithLocation(latLng);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // Handle provider enabled event
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // Handle provider disabled event
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // Handle status changed event
+    }
+
+    // Other methods...
 }
